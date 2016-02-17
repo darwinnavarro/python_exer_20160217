7b15e6c871291f1c9f889d6874c1dc6c6028319a
darwinnavarro <d.navarro@klab.com>
2016-02-17 14:24:04 +0800

    Sample login update

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request, session, url_for, redirect, make_response
 app = Flask(__name__)
 
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -18,16 +27,7 @@ def logout():
         session.pop('username', None)
         return redirect(url_for('home'))
 
-# route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+app.secret_key = 'T@guedkghmde78d3yyy143'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
